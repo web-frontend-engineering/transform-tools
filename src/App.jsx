eb074e76b6e59ed95f80f93fa2ef0e0684de8d60
KEFU\zengweibin <596659597@qq.com>
2025-05-13 15:20:06 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {HashRouter, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
 import Router from './router/index.jsx';
 import Layout from './layout/index.jsx';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Layout>
         <Router/>
       </Layout>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
