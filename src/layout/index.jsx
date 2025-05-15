53ad93eeb6e1a2fbb87911eca8e3dda59a4f2a75
KEFU\zengweibin <596659597@qq.com>
2025-05-15 10:19:35 +0800

    update

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import './index.css'
+import {Link, useLocation} from 'react-router-dom';
+import './index.css';
 
 export default function Layout({children}) {
+  const location = useLocation();
   return (
     <div className="app-container">
       <header className="app-header">
-        <h1>欢迎使用 转换工具</h1>
+        <Link to="/" style={{textDecoration: 'none', color: 'inherit', cursor: 'pointer'}}>
+          <h1>欢迎使用 转换工具</h1>
+        </Link>
       </header>
       <div className="app-layout">
         <aside className="app-sider">
           <nav>
-            <Link to="/object-to-css" className="menu-link">对象 转 CSS样式</Link>
-            <Link to="/object-to-type" className="menu-link">对象 转 类型声明</Link>
+            <Link
+              to="/object-to-css"
+              className={`menu-link${location.pathname === '/object-to-css' ? ' active' : ''}`}
+            >
+              对象 转 CSS样式
+            </Link>
+            <Link
+              to="/object-to-type"
+              className={`menu-link${location.pathname === '/object-to-type' ? ' active' : ''}`}
+            >
+              对象 转 类型声明
+            </Link>
           </nav>
         </aside>
         <main className="app-main">
@@ -22,3 +35,4 @@ export default function Layout({children}) {
     </div>
   );
 }
+
